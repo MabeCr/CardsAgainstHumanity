9501cb32868eed4b6928b7cc484843017d31e915
MabeCr <MabeCr@miamioh.edu>
2015-12-17 00:10:06 -0500

    As of right now, I can control the program with a client program I wrote earlier. Making progress, people.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,12 @@
  * This is a test comment to see if the VCS works on this project.
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class Main {
@@ -22,85 +27,97 @@ public class Main {
     public static int playerNumber;
 
     public static void main(String[] args) {
-        //Create basic white and black card decks
-        Deck whiteDeck = new Deck(white);
-        Deck blackDeck = new Deck(black);
 
-        Scanner scanner = new Scanner(System.in);
+        int portNumber = Integer.parseInt(args[0]);
 
-        //Create hand to store 7 cards in
-        Hand hand1 = new Hand("one", whiteDeck);
-        Hand hand2 = new Hand("two", whiteDeck);
-        Hand hand3 = new Hand("three", whiteDeck);
-        Hand hand4 = new Hand("four", whiteDeck);
+        try(ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
 
-        Hand[] players = new Hand[4];
-        //Setting this manually for testing
-        playerNumber = 4;
-        players[0] = hand1;
-        players[1] = hand2;
-        players[2] = hand3;
-        players[3] = hand4;
 
-        //Get the custom card pack name from the user
-        String packName = getPackName(scanner);
-        System.out.println("");
+            //Create basic white and black card decks
+            Deck whiteDeck = new Deck(white);
+            Deck blackDeck = new Deck(black);
 
-        //Add the custom white and black card to the existing decks
-        addPacksToDeck(packName, whiteDeck, blackDeck);
+            Scanner scanner = new Scanner(System.in);
 
-        //Shuffle both decks to mix the cards up
-        whiteDeck.shuffle();
-        blackDeck.shuffle();
+            //Create hand to store 7 cards in
+            Hand hand1 = new Hand("one", whiteDeck);
+            Hand hand2 = new Hand("two", whiteDeck);
+            Hand hand3 = new Hand("three", whiteDeck);
+            Hand hand4 = new Hand("four", whiteDeck);
 
-        //Deal 7 cards to the hands and then print them out to the screen
-        for (Hand h : players) {
-            System.out.println("Player " + h.getHandName());
-            h.dealHand(whiteDeck);
-            h.printHand();
-            System.out.println("");
-        }
+            Hand[] players = new Hand[4];
+            //Setting this manually for testing
+            playerNumber = 4;
+            players[0] = hand1;
+            players[1] = hand2;
+            players[2] = hand3;
+            players[3] = hand4;
 
-        do {
-            //Draw one black card that will be the for the first round
-            Card currentBlackCard = blackDeck.draw();
-            System.out.println("");
-            System.out.println(currentBlackCard.getString());
+            //Get the custom card pack name from the user
+            String packName = getPackName(scanner);
             System.out.println("");
 
-            //Is only 3, because one person is a judge
-            Card[] choices = new Card[3];
+            //Add the custom white and black card to the existing decks
+            addPacksToDeck(packName, whiteDeck, blackDeck);
 
-            for (int i = 0; i < playerNumber; i++) {
-                if(i != judgeId) {
-                    if(i > judgeId) {
-                        choices[i - 1] = players[i].playCard(0);
-                    }
-                    else
-                        choices[i] = players[i].playCard(0);
-                }
-            }
+            //Shuffle both decks to mix the cards up
+            whiteDeck.shuffle();
+            blackDeck.shuffle();
 
-            for (Card c : choices) {
-                if(c.getString() != null)
-                    System.out.println(c.getString());
-            }
-
-            int winner = chooseWinner(judgeId, players, scanner);
-            System.out.println("The winner is: " + winner);
-            System.out.println("");
-            players[winner].handWins();
-
+            //Deal 7 cards to the hands and then print them out to the screen
             for (Hand h : players) {
-                System.out.println("Player " + h.getHandName() + " has " + h.getWins() + " wins.");
+                System.out.println("Player " + h.getHandName());
+                h.dealHand(whiteDeck);
                 h.printHand();
                 System.out.println("");
             }
 
-            judgeId = (judgeId + 1) % playerNumber;
-        } while (!playerWins(players));
+            do {
+                //Draw one black card that will be the for the first round
+                Card currentBlackCard = blackDeck.draw();
+                System.out.println("");
+                System.out.println(currentBlackCard.getString());
+                System.out.println("");
 
-        scanner.close();
+                //Is only 3, because one person is a judge
+                Card[] choices = new Card[3];
+
+                for (int i = 0; i < playerNumber; i++) {
+                    if (i != judgeId) {
+                        if (i > judgeId) {
+                            choices[i - 1] = players[i].playCard(0);
+                        } else
+                            choices[i] = players[i].playCard(0);
+                    }
+                }
+
+                for (Card c : choices) {
+                    if (c.getString() != null)
+                        System.out.println(c.getString());
+                }
+
+                int winner = chooseWinner(judgeId, players, in);
+                System.out.println("The winner is: " + winner);
+                System.out.println("");
+                players[winner].handWins();
+
+                for (Hand h : players) {
+                    System.out.println("Player " + h.getHandName() + " has " + h.getWins() + " wins.");
+                    h.printHand();
+                    System.out.println("");
+                }
+
+                judgeId = (judgeId + 1) % playerNumber;
+            } while (!playerWins(players));
+
+            scanner.close();
+        } catch (IOException e){
+            System.err.println("Exception caught when listening on port " + portNumber +
+                " or listening for a connection");
+        }
     }
 
     public static String getPackName(Scanner scan) {
@@ -112,19 +129,44 @@ public class Main {
         //If the user enters a pack name
         if (!packName.equals("")) {
             //Set file to point to custom pack white card
-            File packLoadWhite = new File("src\\Cards\\" + packName + "WhiteCards.txt");
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // This is set so I can run the code from cmd to test whether or not the sockets are working
+            // To run in the IDE, set the commented out file to be the used one
+            //File packLoadWhite = new File("src\\Cards\\" + packName + "WhiteCards.txt");
+            File packLoadWhite = new File("Cards\\" + packName + "WhiteCards.txt");
             //Add the custom pack white cards to the original deck
             whiteDeck.addCards(packLoadWhite, white);
-            //Set file to point to c
-            File packLoadBlack = new File("src\\Cards\\" + packName + "BlackCards.txt");
+            //Set file to point to custom pack black card
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // This is set so I can run the code from cmd to test whether or not the sockets are working
+            // To run in the IDE, set the commented out file to be the used one
+            //File packLoadBlack = new File("src\\Cards\\" + packName + "BlackCards.txt");
+            File packLoadBlack = new File("Cards\\" + packName + "BlackCards.txt");
             blackDeck.addCards(packLoadBlack, black);
         }
     }
 
-    public static int chooseWinner(int judgeId, Hand[] players, Scanner scan) {
+    public static int chooseWinner(int judgeId, Hand[] players, BufferedReader in) {
         System.out.println("Player " + players[judgeId].getHandName() + " is the judge.");
         System.out.print("Please enter your winner by typing 1, 2, or 3: ");
-        return Integer.parseInt(scan.nextLine()) - 1;
+        String returnString;
+        int returnValue;
+
+        try{
+            while (true) {
+                System.out.println("Waiting for answer");
+                if ((returnString = in.readLine()) != null) {
+                    System.out.println("Input is not null, it is " + returnString);
+                    returnValue = Integer.parseInt(returnString);
+                    break;
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Could not receive your input");
+            returnValue = 0;
+        }
+
+        return returnValue;
     }
 
     public static boolean playerWins(Hand[] players) {
